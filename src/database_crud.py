e984478a1fbb3561a4ce43f7d714ccc16054ea3c
chris karvouniaris <christos.karvouniaris247@gmail.com>
2022-11-30 14:02:56 +0200

    changed db models; added models user and watchlist

--- src/database_crud.py
+++ src/database_crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from db_models import Movies
+from db_models import Movie
 from schemas import Movie, MovieWatch
 from datetime import datetime
 
@@ -9,7 +9,7 @@ class TooSoonException(Exception):
 
 
 def add_movie(db: Session, movie: Movie):
-    db_movie = Movies(title=movie.title, release_date=movie.release_date)
+    db_movie = Movie(title=movie.title, release_date=movie.release_date)
     db.add(db_movie)
     db.commit()
     db.refresh(db_movie)
@@ -17,12 +17,12 @@ def add_movie(db: Session, movie: Movie):
 
 
 def get_movies(db: Session):
-    movies = list(db.query(Movies).all())
+    movies = list(db.query(Movie).all())
     return movies
 
 
 def update_movie(db: Session, movie_id: int, watch: MovieWatch):
-    movie_cursor = db.query(Movies).filter(Movies.id == movie_id)
+    movie_cursor = db.query(Movie).filter(Movie.id == movie_id)
     if not movie_cursor.first():
         raise ValueError(
             f"There is no movie with ID {movie_id} in the watchlist")
@@ -32,21 +32,21 @@ def update_movie(db: Session, movie_id: int, watch: MovieWatch):
             f"This movie cannot be watched as it isn't released yet!")
     movie_cursor.update(
         {
-            Movies.watched: watch.value,
-            Movies.watched_date: datetime.now() if watch.value else None
+            Movie.watched: watch.value,
+            Movie.watched_date: datetime.now() if watch.value else None
         }
     )
     db.commit()
-    return db.query(Movies).filter(Movies.id == movie_id).first()
+    return db.query(Movie).filter(Movie.id == movie_id).first()
 
 
 def get_watched_movies(db: Session):
-    return list(db.query(Movies).filter(Movies.watched == True).all())
+    return list(db.query(Movie).filter(Movie.watched == True).all())
 
 
 def get_upcoming_movies(db: Session):
-    return list(db.query(Movies).filter(Movies.release_date > datetime.now()).all())
+    return list(db.query(Movie).filter(Movie.release_date > datetime.now()).all())
 
 
 def search_movies(db: Session, keyword: str):
-    return list(db.query(Movies).filter(Movies.title.contains(keyword)).all())
+    return list(db.query(Movie).filter(Movie.title.contains(keyword)).all())
--- src/db_models.py
+++ src/db_models.py
@@ -1,14 +1,37 @@
-from sqlalchemy import Column, DateTime, Integer, String, Boolean
+from sqlalchemy import Column, DateTime, Integer, String, Boolean, ForeignKey, PrimaryKeyConstraint
 from sqlalchemy.sql import func
 from database import Base
+from sqlalchemy.orm import relationship
 
-
-class Movies(Base):
+class Movie(Base):
     __tablename__ = "movies"
     # autoincreament id for each movie insertion
     id = Column(Integer, primary_key=True, index=False)
     title = Column(String)
     release_date = Column(DateTime)
+
+    watchlist = relationship("Watchlist", back_populates="movie")
+
+class User(Base):
+    __tablename__ = "users"
+    email = Column(String, primary_key=True, index=False)
+    username = Column(String)
+    password = Column(String)
+    register_date = Column(DateTime, default=func.now())
+    register_activated = Column(Boolean, default=False)
+
+    watchlist = relationship("Watchlist", back_populates="user")
+
+class Watchlist(Base):
+    __tablename__ = "watchlists"
+    movie_id = Column(Integer, ForeignKey("movies.id"))
+    user_email = Column(String, ForeignKey("users.email"))
+    __table_args__ = (
+        PrimaryKeyConstraint("movie_id", "user_email", name="user_email_movie")
+    )
     added_in_watchlist = Column(DateTime, default=func.now())
     watched = Column(Boolean, default=False)
     watched_date = Column(DateTime)
+
+    movie = relationship("Movie", back_populates="watchlist")
+    user = relationship("User", back_populates="watchlist")
